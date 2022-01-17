e2071af6dde876dd126fe755a6b1a52af6a8c547
heynarmatha <hey.narmatha@gmail.com>
2022-01-17 17:22:57 +0530

    First commit

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+const  smallImage =  document.querySelectorAll(".gallery img");
+const model = document.querySelector(".model");
+const largeImage = document.querySelector(".full-img");
+
+smallImage.forEach(function(img){
+    img.addEventListener("click" , function(){
+        model.classList.add("open");
+        largeImage.classList.add("open");
+        // dynamic 
+        const original = img.getAttribute("alt");
+        largeImage.src = `../../public/images/full/${original}.jpg`;
+    });
+});
+
+model.addEventListener("click",function(e){
+    if(e.target.classList.contains("model")){
+        model.classList.remove("open");
+        largeImage.classList.remove("open"); 
+    }
+});
